8e19a1431c8d2bd223f55313a6e36fdf9a56df2f
Jose Munoz <jmunoz@jumpitt.com>
2023-06-07 14:41:08 -0400

    commit desde mac

--- app/src/main/java/com/example/magnapp/MainActivity.kt
+++ app/src/main/java/com/example/magnapp/MainActivity.kt
@@ -11,6 +11,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            // TEST
+            val prueba = "hola prueba"
             MagnAppTheme {
                 Navigation()
             }
